e53edbc2cc3b3075448eaf8282f11a911255f1be
Basavanna <basavannatej25@gmail.com>
2024-08-30 13:51:50 +0530

    added relationship table between car and feature

--- lib/db/models.py
+++ lib/db/models.py
@@ -4,12 +4,29 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+car_manufacturer = Table(
+    'car_manufacturer',
+    Base.metadata,
+    Column('car_id', Integer, ForeignKey('cars.id'), primary_key = True),
+    Column('manufacturer_id', Integer, ForeignKey('manufacturers.id'), primary_key = True)
+)
+
+car_feature = Table(
+    'car_feature',
+    Base.metadata,
+    Column('car_id', Integer, ForeignKey('cars.id'), primary_key = True),
+    Column('feature_id', Integer, ForeignKey('features.id'), primary_key = True)
+)
+
+
+
 class Car(Base):
     __tablename__ = 'cars'
     id = Column(Integer, primary_key = True)
     name = Column(String, nullable = False)
     price = Column(Integer)
     horspower = Column(Integer)
+    
     manufacturers = relationship('Manufacturer', secondary = car_manufacturer, back_populates = 'cars')
     features = relationship('Feature', secondary = car_feature, back_populates= 'cars')
 
@@ -17,6 +34,7 @@ class Manufacturer(Base):
     __tablename__ = 'manufacturers'
     id = Column(Integer, orimary_key = True)
     name = Column(String, nullable= False)
+    
     cars = relationship('Car', secondary = car_manufacturer, back_populates = 'manufacturers')
 
 class Feature(Base):
