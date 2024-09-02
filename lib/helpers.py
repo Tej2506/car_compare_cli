a363fe5d5d2d310467eb93cf9e6085d688932405
Basavanna <basavannatej25@gmail.com>
2024-09-02 12:03:30 +0530

    finished web scraping for tables car and manufacturer

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,23 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
-from lib.db.models import Car, Manufacturer, session
+from db.models import Car, Manufacturer, session
 
 def scrape_car_details(manufacturer, car_name):
-    manufacturer_search_query = brand.replace(" ", "-").lower()
+    manufacturer_search_query = manufacturer.replace(" ", "-").lower()
     car_name_search_query = car_name.replace(" ", "-").lower()
-    url = f"https://www.cardekho.com/{manufacturer_search_query}/{car_name_search_query}"
 
-    response = requests.get(url)
+    url = f"https://www.cardekho.com/{manufacturer_search_query}/{car_name_search_query}"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(url, headers = headers)
     if response.status_code != 200:
         print(f"Failed to retrieve data fro {car_name}")
         return None
 
     soup = BeautifulSoup(response.text,'html.parser')
     car_details = {}
-    car_details['name'] = soup.find('h1', class_='thcHeading').text
+    car_details['name'] = car_name
     car_details['manufacturer'] = manufacturer
-    car_details['power'] = soup.find('td', class_= 'iconsname').text
-    car_details['price'] = soup.find('i', class_= 'icon-cd_R').text
+    car_details['price'] = soup.find('div', class_= 'price').text.split('Rs.')[1].split('*')[0]
+    tr_elements = soup.find('div', class_="qccontent").find_all('tr')
+    car_specs=["Engine", "Power", "Torque"]
+    
+    for row in tr_elements:
+        car_spec = row.find_all('td')[0].text.strip()
+        car_spec_value = row.find_all('td')[1].text.strip()
+        if car_spec in car_specs:
+            car_details[car_spec] = car_spec_value
+    
     return car_details
 
 def save_car_details_to_db(car_details):
@@ -26,17 +36,30 @@ def save_car_details_to_db(car_details):
 
     manufacturer_name = car_details['manufacturer']
     car_name = car_details['name']
-    car_price = car_details['price']
-    car_power = car_details['power']
+    car_price = car_details.get('price') 
+    car_power = car_details.get('Power')
+    car_engine = car_details.get('Engine')
+    car_torque = car_details.get('Torque')
+    
 
-    manufacturer = session.query(Manufacturer).filter_by(name=manufacturer_name).first()
+    manufacturer = session.query(Manufacturer).filter_by(name=manufacturer_name.lower()).first()
     if not manufacturer:
         manufacturer = Manufacturer(name = manufacturer_name)
         session.add(manufacturer)
         session.commit()
     
-    car = session.query(Car).filter_by(name=car_name, manufacturer=manufacturer).first()
+    car = session.query(Car).filter_by(name=car_name.lower()).first()
     if not car:
-        car = Car(name=car_name, price=car_price, power=car_power)
+        car = Car(
+            name=car_name, 
+            price=car_price, 
+            power=car_power, 
+            engine=car_engine, 
+            torque=car_torque
+        )
         session.add(car)
         session.commit()
+    session.close()
+
+
+
